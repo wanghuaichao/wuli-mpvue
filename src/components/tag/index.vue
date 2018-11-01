aba2d996c7abd4a8b5083bbfa42acd9eed92e19e
wanghuaichao <2546714411@qq.com>
2018-11-01 16:16:36 +0800

    no message

---
+++ src/components/tag/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <span class="wuli-tag"
+        >
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        size: String,
+        type: String,
+        mark: Boolean,
+        color: String,
+        plain: Boolean,
+        round: Boolean
+  },
+  data() {
+        return {
+            
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+
+        }
+}
+</script>
--- src/pages/tag/index.vue
+++ src/pages/tag/index.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="container">
-        
+        <div>基础用法</div>
+        <wp-tag>标签一</wp-tag>
+        <wp-tag type="success">标签二</wp-tag>
+        <wp-tag type="info">标签三</wp-tag>
+        <wp-tag type="warning">标签四</wp-tag>
+        <wp-tag type="danger">标签五</wp-tag>
     </div>
 </template>
 
 <script>
-
+import wpTag from '@/components/tag'
 export default {
-    data() {
-        return {
-            
-        }
-    },
-    onShow() {
-       
-    },
-    methods: {
-       
-    },
-}
+  data() {
+    return {};
+  },
+  onShow() {},
+  methods: {},
+  components: {
+      wpTag
+  }
+};
 </script>
