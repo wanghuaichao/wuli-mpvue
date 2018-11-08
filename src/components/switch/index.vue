bc32091e62f93761bd36ba2be8611e86b6d9bf38
wanghuaichao <2546714411@qq.com>
2018-11-08 18:56:43 +0800

    no message

--- src/components/switch/index.vue
+++ src/components/switch/index.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="wuli-switch" :class="[
-    checked ?'wuli-switch__on':'wuli-switch__off'
+    checked ?'wuli-switch__on':'wuli-switch__off',
+    text ?'wuli-switch__text':''
     ]" @click="click">
         <div class="wuli-switch_word" v-if="text">{{text==1?'on':'开'}}</div>
-        <div class="wuli-switch__circle"></div>
-        <div class="wuli-switch__bg"><span v-if="text">{{text==1?'off':'关'}}</span></div>
+        <div class="wuli-switch__circle" :class="[text?'wuli-switch__circle2':'']"></div>
+        <div class="wuli-switch__bg" :class="[text?'wuli-switch__bg2':'']"><span v-if="text">{{text==1?'off':'关'}}</span></div>
     </div>
 </template>
 
@@ -12,7 +13,8 @@
 export default {
     props:{
      checked:Boolean,
-     text:Number
+     text:Number,
+     color:String
    },
   data() {
     return {};
