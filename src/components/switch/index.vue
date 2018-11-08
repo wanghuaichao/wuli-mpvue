dd4b6c722528c7b97fb8b60379b63fa86f1473b7
wanghuaichao <2546714411@qq.com>
2018-11-08 17:32:54 +0800

    no message

--- src/components/switch/index.vue
+++ src/components/switch/index.vue
@@ -1,26 +1,31 @@
 <template>
- <div>
-
- </div>
+    <div class="wuli-switch" :class="[
+    checked ?'wuli-switch__on':'wuli-switch__off'
+    ]" @click="click">
+        <div class="wuli-switch_word" v-if="text">{{text==1?'on':'开'}}</div>
+        <div class="wuli-switch__circle"></div>
+        <div class="wuli-switch__bg"><span v-if="text">{{text==1?'off':'关'}}</span></div>
+    </div>
 </template>
 
 <script>
- export default {
-   data () {
-     return {
-
-     }
+export default {
+    props:{
+     checked:Boolean,
+     text:Number
    },
-   methods: {
-
-   },
-   components: {
-
-   }
- }
+  data() {
+    return {};
+  },
+  methods: {
+      click(){
+          this.checked=!this.checked
+          this.$emit('change',this.checked);
+      }
+  },
+  components: {}
+};
 </script>
 
 <style>
-
- 
 </style>
